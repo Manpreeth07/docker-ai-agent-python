1d671bca6ec2e96485548db3cb960472c8d4aa6d
Manpreeth <vankadaramanu16@gmail.com>
2026-07-17 17:47:28 +0200

    Docker model runner

--- backend/src/api/chat/routing.py
+++ backend/src/api/chat/routing.py
@@ -2,6 +2,8 @@ from typing import List
 from fastapi import APIRouter,Depends
 from sqlmodel import Session, select
 from api.db import get_session
+from api.ai.schemas import EmailMessageSchema
+from api.ai.services import generate_email_message
 from .models import ChatMessagePayload,ChatMessage,ChatMessageListItem
 router = APIRouter()
 
@@ -21,7 +23,9 @@ def chat_list_messages(session: Session = Depends(get_session)):
 
 #HTTP POST-> payload ={"message":"Hello world"}->{"message":"hello world","id":1}
 #curl -X POST -d '{"message":"Hello world"}' -H "Content-Type: application/json" http://localhost:8080/api/chats/
-@router.post("/", response_model=ChatMessageListItem)
+#curl -X POST -d '{"message":"Hello world"}' -H "Content-Type: application/json" https://docker-api-fastapi-python-app-nw8lv.ondigitalocean.app/api/chats/
+#curl -X POST -d '{"message":"GIve me a summary of why it is good to go outside"}' -H "Content-Type: application/json" http://localhost:8080/api/chats/
+@router.post("/", response_model=EmailMessageSchema)
 def chat_create_message(
     payload: ChatMessagePayload,
     session: Session = Depends(get_session)
@@ -31,6 +35,7 @@ def chat_create_message(
     obj=ChatMessage.model_validate(data)
     session.add(obj)
     session.commit()
-    session.refresh(obj)
+    # session.refresh(obj)
     #ready to store in database
-    return obj
+    response=generate_email_message(payload.message)
+    return response
